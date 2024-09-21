ab834fda282b9d842056c90f8052f20b448fa080
Anna Teilane <kastekastee@gmail.com>
2024-09-21 18:47:50 +0000

    js: add click event listeners to music control elements

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,4 +9,16 @@ function musicControls() {
     const backgroundMusic = document.getElementById('background-music');
     const playButton = document.getElementById('play-btn');
     const pauseButton = document.getElementById('pause-btn');
+    
+    if (playButton) {
+        playButton.addEventListener('click', () => {
+            backgroundMusic.play();
+        });
+    }
+
+    if (pauseButton) {
+        pauseButton.addEventListener('click', () => {
+            backgroundMusic.pause();
+        });
+    }
 }
