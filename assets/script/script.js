ae7cbbc3d2665d5bf43f95b2d6a5eb096abebbfe
Anna Teilane <kastekastee@gmail.com>
2024-09-21 20:29:21 +0000

    js: wrap all game code in a function

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,42 +23,45 @@ function musicControls() {
     }
 }
 
-var game = document.getElementById('game');
-const cards = [
-    {src: 'https://i.ibb.co/YRRFR6v/warrior.webp', alt: 'warrior'},
-    {src: 'https://i.ibb.co/QkWbTqJ/silent-angel.webp', alt: 'silent angel'},
-    {src: 'https://i.ibb.co/0YX9b8d/sauron.webp', alt: 'sauron'},
-    {src: 'https://i.ibb.co/V01BQWN/goddess.webp', alt: 'goddess'},
-    {src: 'https://i.ibb.co/1z6nmgJ/ghost.webp', alt: 'ghost'},
-    {src: 'https://i.ibb.co/cQR9PfW/druid.webp', alt: 'druid'},
-    {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel'},
-    {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel'},
-    {src: 'https://i.ibb.co/YRRFR6v/warrior.webp', alt: 'warrior'},
-    {src: 'https://i.ibb.co/QkWbTqJ/silent-angel.webp', alt: 'silent angel'},
-    {src: 'https://i.ibb.co/0YX9b8d/sauron.webp', alt: 'sauron'},
-    {src: 'https://i.ibb.co/V01BQWN/goddess.webp', alt: 'goddess'},
-    {src: 'https://i.ibb.co/1z6nmgJ/ghost.webp', alt: 'ghost'},
-    {src: 'https://i.ibb.co/cQR9PfW/druid.webp', alt: 'druid'},
-    {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel'},
-    {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel'}
-];
-cards.sort(
-    /** Shuffle cards */
-    function shuffle(){return 0.5 - Math.random()
-    });
-
-    cards.forEach(
-        /** Display card images */
-        function display(cards) {
-            var cardDiv = `<div class='card'><img src='${cards.src}' alt='${cards.alt}'/></div>`;
-            game.innerHTML += cardDiv;
+/** Load cards */
+function loadCards(){
+    var game = document.getElementById('game');
+    const cards = [
+        {src: 'https://i.ibb.co/YRRFR6v/warrior.webp', alt: 'warrior'},
+        {src: 'https://i.ibb.co/QkWbTqJ/silent-angel.webp', alt: 'silent angel'},
+        {src: 'https://i.ibb.co/0YX9b8d/sauron.webp', alt: 'sauron'},
+        {src: 'https://i.ibb.co/V01BQWN/goddess.webp', alt: 'goddess'},
+        {src: 'https://i.ibb.co/1z6nmgJ/ghost.webp', alt: 'ghost'},
+        {src: 'https://i.ibb.co/cQR9PfW/druid.webp', alt: 'druid'},
+        {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel'},
+        {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel'},
+        {src: 'https://i.ibb.co/YRRFR6v/warrior.webp', alt: 'warrior'},
+        {src: 'https://i.ibb.co/QkWbTqJ/silent-angel.webp', alt: 'silent angel'},
+        {src: 'https://i.ibb.co/0YX9b8d/sauron.webp', alt: 'sauron'},
+        {src: 'https://i.ibb.co/V01BQWN/goddess.webp', alt: 'goddess'},
+        {src: 'https://i.ibb.co/1z6nmgJ/ghost.webp', alt: 'ghost'},
+        {src: 'https://i.ibb.co/cQR9PfW/druid.webp', alt: 'druid'},
+        {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel'},
+        {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel'}
+    ];
+    cards.sort(
+        /** Shuffle cards */
+        function shuffle(){return 0.5 - Math.random()
         });
 
-        var card = document.querySelectorAll('.card');
-        /* Click event listener */
-        card.forEach((card) => card.addEventListener('click', openCard));
-        
-        /** Open card */
-        function openCard(){
-            this.classList.add('cardOpen');
+        cards.forEach(
+            /** Display card images */
+            function display(cards) {
+                var cardDiv = `<div class='card'><img src='${cards.src}' alt='${cards.alt}'/></div>`;
+                game.innerHTML += cardDiv;
+            });
+
+            var card = document.querySelectorAll('.card');
+            /* Click event listener */
+            card.forEach((card) => card.addEventListener('click', openCard));
+            
+            /** Open card */
+            function openCard(){
+                this.classList.add('cardOpen');
+            }
         }
