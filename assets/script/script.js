8c8aab97c0e87a15c329f12a9d251c2a010f58c0
Anna Teilane <kastekastee@gmail.com>
2024-09-21 19:02:29 +0000

    js: create forEach loop to display cards wrapped in divs

--- assets/script/script.js
+++ assets/script/script.js
@@ -46,3 +46,10 @@ cards.sort(
     /** Shuffle cards */
     function shuffle(){return 0.5 - Math.random()
     });
+    
+    cards.forEach(
+        /** Display card images */
+        function display(cards) {
+            var cardDiv = `<div class='card'><img src='${cards.src}' alt='${cards.alt}'/></div>`;
+            game.innerHTML += cardDiv;
+        });
